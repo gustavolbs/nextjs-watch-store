e6ebc983d42edbc3d9e4b77f0e1e37c9baa61af9
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-23 20:13:35 -0300

    test index page

---
+++ pages-tests/product-list.integration.spec.js
@@ -0,0 +1,89 @@
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import ProductList from '../pages/index';
+import { makeServer } from '../miragejs/server';
+import { Response } from 'miragejs';
+import userEvent from '@testing-library/user-event';
+
+const renderProductList = () => {
+  render(<ProductList />);
+};
+
+describe('ProductList', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  it('should render the component', () => {
+    renderProductList();
+
+    expect(screen.getByTestId('product-list')).toBeInTheDocument();
+  });
+
+  it('should render the ProductCard component 10 times', async () => {
+    server.createList('product', 10);
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10);
+    });
+  });
+
+  it('should render the "no products" message', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+      expect(screen.getByTestId('no-products').textContent).toEqual('No products');
+    });
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, '');
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument();
+      expect(screen.getByTestId('server-error').textContent).toEqual('Oops! Something went wrong');
+      expect(screen.queryByTestId('no-products')).toBeNull();
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
+    });
+  });
+
+  it('should filter the product list when a search is performed', async () => {
+    const searchTerm = 'Relógio bonito';
+    server.createList('product', 2);
+    server.create('product', {
+      title: searchTerm,
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(3);
+    });
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(1);
+    });
+  });
+
+  it.todo('should display the total quantity of products');
+
+  it.todo('should display product (singular) when there is only 1 product');
+});
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
+import { useState, useEffect } from 'react';
 import ProductCard from '../components/product-card';
 import Search from '../components/search';
+import { useFetchProducts } from '../hooks/use-fetch-products';
 
 export default function Home() {
+  const [term, setTerm] = useState('');
+  const [localProducts, setLocalProducts] = useState([]);
+  const { products, error } = useFetchProducts();
+
+  useEffect(() => {
+    if (term === '') {
+      setLocalProducts(products);
+    } else {
+      setLocalProducts(
+        products.filter(({ title }) => {
+          return title.toLowerCase().includes(term.toLowerCase());
+        }),
+      );
+    }
+  }, [products, term]);
+
+  const renderProductListOrMessage = () => {
+    if (error) {
+      return <h4 data-testid="server-error">Oops! Something went wrong</h4>;
+    }
+
+    if (localProducts.length === 0) {
+      return <h4 data-testid="no-products">No products</h4>;
+    }
+
+    return localProducts.map((product) => <ProductCard key={product.id} product={product} />);
+  };
+
   return (
-    <main className="my-8">
-      <Search />
+    <main data-testid="product-list" className="my-8">
+      <Search doSearch={(term) => setTerm(term)} />
       <div className="container mx-auto px-6">
         <h3 className="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
         <span className="mt-3 text-sm text-gray-500">200+ Products</span>
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          <ProductCard />
+          {renderProductListOrMessage()}
         </div>
       </div>
     </main>
