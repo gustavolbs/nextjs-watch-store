09753bff1d34e6898fc02ebba6cde53eb4ac3c3a
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-05-23 20:13:08 -0300

    fix mispelling

--- components/cart.js
+++ components/cart.js
@@ -20,7 +20,7 @@ export default function Cart() {
         </button>
       </div>
       <hr className="my-3" />
-      <CartItem />
+      <CartItem product={{}} />
       <div className="mt-8">
         <form className="flex items-center justify-center">
           <input className="form-input w-48" type="text" placeholder="Add promocode" />
@@ -30,7 +30,7 @@ export default function Cart() {
         </form>
       </div>
       <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-        <span>Chechout</span>
+        <span>Checkout</span>
         <svg
           className="h-5 w-5 mx-2"
           fill="none"
